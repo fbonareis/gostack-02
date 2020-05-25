54ead9db00bbfc1f98c32bb7b691b2cf9d61799a
Fernando de Bona dos Reis <freis@unimedcascavel.coop.br>
2020-05-25 14:57:48 -0300

    resolve tests

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,72 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+app.get("/repositories", async (request, response) => {
+  return response.json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post("/repositories", async (request, response) => {
+  const { url, title, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    url,
+    title,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", async (request, response) => {
+  const { id } = request.params;
+
+  const currentIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (currentIndex === -1) {
+    return response.status(400).json("repository does not exist.");
+  }
+
+  const { url, title, techs } = request.body;
+
+  repositories[currentIndex].url = url;
+  repositories[currentIndex].title = title;
+  repositories[currentIndex].techs = techs;
+
+  return response.json(repositories[currentIndex]);
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
+app.delete("/repositories/:id", async (request, response) => {
+  const { id } = request.params;
+
+  const index = repositories.findIndex((repository) => repository.id === id);
+
+  if (index === -1) {
+    return response.status(400).json("repository that not exist.");
+  }
+
+  repositories.splice(index, 1);
+
+  return response.status(204).json({});
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", async (request, response) => {
+  const { id } = request.params;
+
+  const index = repositories.findIndex((repository) => repository.id === id);
+
+  if (index === -1) {
+    return response.status(400).json({});
+  }
+
+  repositories[index].likes = repositories[index].likes + 1;
+
+  return response.json(repositories[index]);
 });
 
 module.exports = app;
